665caba09418e7e16812d9fb1b8970a66461194a
Marine Dunstetter <marine.dunstetter@ukg.com>
2021-10-04 18:58:03 +0200

    feat: factorize projects slot, add slots for PeopleDoc

--- app/models/category.js
+++ app/models/category.js
@@ -2,7 +2,7 @@ import Model, { attr } from '@ember-data/model';
 
 export default class CategoryModel extends Model {
   @attr('string') keyroute;
-  @attr('string') name;
+  @attr('string') slotRoute;
   @attr('string') sprite;
   @attr slots;
   @attr badges;
