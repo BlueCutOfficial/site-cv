82a76496bf663746051502d4bdbb1f0910bbfa7f
Marine Dunstetter <marine.dunstetter@ukg.com>
2022-03-27 22:08:25 +0200

    Various refactors (#23)
    * refactor: rename equipment bar to menu bar, as the theme of the app is no longer video games
    
    * refactor: clean up glimmer approach in category-item
    
    * refactor: rename equiment-item to menu-bar-item, as the theme of the app is no longer video games
    
    * refactor: rename category-item to category, as item sounds more like a unit into the category
    
    * fix: remove unused information in used fixtures

--- app/components/category.js
+++ app/components/category.js
@@ -1,33 +1,24 @@
 /* eslint-disable ember/no-computed-properties-in-native-classes */
 import Component from '@glimmer/component';
-import { computed } from '@ember/object';
 
 // Categoies can show slots and/or badges
-export default class CategoryItemComponent extends Component {
-  model = this.args.model;
-
-  // True if there are badges to show
-  @computed('model.badges.[]')
+export default class CategoryComponent extends Component {
   get hasBadges() {
-    let badges = this.model.badges;
+    let badges = this.args.model.badges;
     return badges !== undefined && badges.length > 0;
   }
 
-  //  True if there are slots to show
-  @computed('model.slots.[]')
   get hasSlots() {
-    let slots = this.model.slots;
+    let slots = this.args.model.slots;
     return slots !== undefined && slots.length > 0;
   }
 
   // Change bootstrap grid according to the case, 2 columns if needed
-  @computed('hasSlots', 'hasBadges')
   get colClassSlots() {
     return this.hasSlots && this.hasBadges ? 'col-md-5' : '';
   }
 
   // For badges case, add borderleft aside with col-md-6 to make the separator
-  @computed('hasSlots', 'hasBadges')
   get colClassBadge() {
     return this.hasSlots && this.hasBadges ? 'borderleft col-md-6' : '';
   }
