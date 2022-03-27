c8645d805b721ed2f0f0f15f56d950902dbd93a4
Marine Dunstetter <marine.dunstetter@ukg.com>
2022-03-27 18:30:29 +0200

    docs(category-item): fix old french comments to english

--- app/components/category-item.js
+++ app/components/category-item.js
@@ -2,30 +2,31 @@
 import Component from '@glimmer/component';
 import { computed } from '@ember/object';
 
-// les catégories affichent des slots et/ou badges
+// Categoies can show slots and/or badges
 export default class CategoryItemComponent extends Component {
   model = this.args.model;
 
-  // Vrai s'il y a des badges à afficher pour cette catégorie
+  // True if there are badges to show
   @computed('model.badges.[]')
   get hasBadges() {
     let badges = this.model.badges;
     return badges !== undefined && badges.length > 0;
   }
 
-  // Vrai s'il y a des slots à afficher pour cette catégorie
+  //  True if there are slots to show
   @computed('model.slots.[]')
   get hasSlots() {
     let slots = this.model.slots;
     return slots !== undefined && slots.length > 0;
   }
 
-  // On change la classe bootstrap selon les listes existantes, pour faire deux colonnes si besoin
+  // Change bootstrap grid according to the case, 2 columns if needed
   @computed('hasSlots', 'hasBadges')
   get colClassSlots() {
     return this.hasSlots && this.hasBadges ? 'col-md-5' : '';
   }
-  // Dans le cas des badges, on ajoute borderleft en plus de col-md-6, pour faire la séparation
+
+  // For badges case, add borderleft aside with col-md-6 to make the separator
   @computed('hasSlots', 'hasBadges')
   get colClassBadge() {
     return this.hasSlots && this.hasBadges ? 'borderleft col-md-6' : '';
