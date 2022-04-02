4aa02e497688ac81195bf24e31e24e083699604c
Marine Dunstetter <marine.dunstetter@ukg.com>
2022-04-02 13:00:12 +0200

    refactor(templates): move the project header to a component

---
+++ tests/integration/components/project-template-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | project-template', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders the title and intro for the given @projectId', async function (assert) {
+    await render(hbs`<ProjectTemplate @projectId="reef" />`);
+    assert.dom('h1').hasText('Reef');
+    assert
+      .dom('p')
+      .exists({ count: 1 })
+      .includesText('A relaxing serious game inspired by Biorock technology.');
+
+    await render(hbs`
+      <ProjectTemplate @projectId="reef">
+        <p class="desc">Project custom description.</p>
+      </ProjectTemplate>
+    `);
+    assert.dom('h1').hasText('Reef');
+    assert.dom('p').exists({ count: 2 });
+    assert.dom('.desc').hasText('Project custom description.');
+  });
+});
