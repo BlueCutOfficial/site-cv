d0102553ddaf63c37b4a02e9dbd76314763ac08b
Marine Dunstetter <marine.dunstetter@ukg.com>
2022-04-08 09:51:36 +0200

    test(profile-header): set up accessibility test

--- tests/integration/components/profile-header-test.js
+++ tests/integration/components/profile-header-test.js
@@ -2,6 +2,7 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { a11yAudit } from 'ember-a11y-testing/test-support';
 
 module('Integration | Component | profile-header', function (hooks) {
   setupRenderingTest(hooks);
@@ -11,5 +12,11 @@ module('Integration | Component | profile-header', function (hooks) {
     assert.dom('.profile-header img').hasAttribute('alt', 'My name');
     assert.dom('.profile-header h1').hasText('My name');
     assert.dom('.profile-header h2').exists({ count: 1 });
+  });
+
+  test('it passes a11yAudit', async function (assert) {
+    await render(hbs`<ProfileHeader @me={{hash name="My name"}} />`);
+    await a11yAudit(this.element);
+    assert.ok(true, 'no a11y errors found');
   });
 });
